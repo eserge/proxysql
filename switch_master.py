b692cc50ae8662698f4690a13dabbc44da6186ea
Tsmyh, Dzmitry <dtsmyh@scnsoft.com>
2022-11-18 11:57:48 +0300

    handle failover

--- switch_master.py
+++ switch_master.py
@@ -1,65 +1,70 @@
 import sys
 
 import mysql.connector
+from mysql.connector.errors import DatabaseError
 
 old_master_host = sys.argv[1]
 new_master_host = sys.argv[2]
 replica_host = "192.168.96.5"
 slavepass = "slavepass"
+db_user = "root"
+db_pass = "secret"
+db_port = 3306
 
 
-merch_db_old_master = mysql.connector.connect(
-    host=old_master_host,
-    port=3306,
-    user="root",
-    password="secret",
-)
+def connect_db(host, port, user, password):
+    db = mysql.connector.connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+    )
+    return db
 
-merch_db_new_master = mysql.connector.connect(
-    host="192.168.96.4",
-    port=3306,
-    user="root",
-    password="secret",
-)
-merch_db_replica = mysql.connector.connect(
-    host=replica_host,
-    port=3306,
-    user="root",
-    password="secret",
-)
 
-merch_cursor = merch_db_old_master.cursor()
-merch_cursor2 = merch_db_new_master.cursor()
-merch_cursor3 = merch_db_replica.cursor()
+try:
+    db_old_master = connect_db(old_master_host, db_port, db_user, db_pass)
+except DatabaseError:
+    print('---Master is dead---')
+else:
+    cursor = db_old_master.cursor()
+    print('---Setting old master to slave status---')
+    cursor.execute("STOP SLAVE;")
+    cursor.execute(f"CHANGE MASTER TO MASTER_HOST='{new_master_host}', MASTER_USER='repl', MASTER_PASSWORD='{slavepass}';")
+    cursor.execute("START SLAVE;")
+    cursor.execute("FLUSH TABLES WITH READ LOCK;")
+    cursor.execute("SET GLOBAL read_only=ON;")
+    cursor.execute("UNLOCK TABLES;")
+    cursor.execute(r"SHOW SLAVE status;")
+    res = cursor.fetchall()
+    print('---Slave status---')
+    print(res)
+
+
+db_new_master = connect_db(new_master_host, db_port, db_user, db_pass)
+db_replica = connect_db(replica_host, db_port, db_user, db_pass)
+
+
+cursor2 = db_new_master.cursor()
+cursor3 = db_replica.cursor()
 print('---Starting set up a new master---')
-merch_cursor2.execute("STOP SLAVE;")
-merch_cursor2.execute("RESET MASTER;")
-merch_cursor2.execute(f"CREATE USER repl@'{old_master_host}' IDENTIFIED VIA mysql_native_password USING PASSWORD('{slavepass}');")
-merch_cursor2.execute(f"GRANT REPLICATION SLAVE ON *.* TO repl@'{old_master_host}';")
-merch_cursor2.execute("FLUSH PRIVILEGES;")
-merch_cursor2.execute("FLUSH TABLES WITH READ LOCK;")
-merch_cursor2.execute("SET GLOBAL read_only=OFF;")
-merch_cursor2.execute("UNLOCK TABLES;")
-merch_cursor2.execute("SHOW MASTER status;")
-res = merch_cursor2.fetchall()
+cursor2.execute("STOP SLAVE;")
+cursor2.execute("RESET MASTER;")
+cursor2.execute(f"CREATE USER repl@'{old_master_host}' IDENTIFIED VIA mysql_native_password USING PASSWORD('{slavepass}');")
+cursor2.execute(f"GRANT REPLICATION SLAVE ON *.* TO repl@'{old_master_host}';")
+cursor2.execute("FLUSH PRIVILEGES;")
+cursor2.execute("FLUSH TABLES WITH READ LOCK;")
+cursor2.execute("SET GLOBAL read_only=OFF;")
+cursor2.execute("UNLOCK TABLES;")
+cursor2.execute("SHOW MASTER status;")
+res = cursor2.fetchall()
 print('---Master status---')
 print(res)
-print('---Setting old master to slave status---')
-merch_cursor.execute("STOP SLAVE;")
-merch_cursor.execute(f"CHANGE MASTER TO MASTER_HOST='{new_master_host}', MASTER_USER='repl', MASTER_PASSWORD='{slavepass}';")
-merch_cursor.execute("START SLAVE;")
-merch_cursor.execute("FLUSH TABLES WITH READ LOCK;")
-merch_cursor.execute("SET GLOBAL read_only=ON;")
-merch_cursor.execute("UNLOCK TABLES;")
-merch_cursor.execute(r"SHOW SLAVE status;")
-res = merch_cursor.fetchall()
-print('---Slave status---')
-print(res)
 print('---Indicating replica to a new master---')
-merch_cursor3.execute("STOP SLAVE;")
-merch_cursor3.execute(f"CHANGE MASTER TO MASTER_HOST='{new_master_host}', MASTER_USER='repl', MASTER_PASSWORD='{slavepass}';")
-merch_cursor3.execute("START SLAVE;")
-merch_cursor3.execute(r"SHOW SLAVE status;")
-res = merch_cursor3.fetchall()
+cursor3.execute("STOP SLAVE;")
+cursor3.execute(f"CHANGE MASTER TO MASTER_HOST='{new_master_host}', MASTER_USER='repl', MASTER_PASSWORD='{slavepass}';")
+cursor3.execute("START SLAVE;")
+cursor3.execute(r"SHOW SLAVE status;")
+res = cursor3.fetchall()
 print('---Slave status---')
 print(res)
--- switchover.sh
+++ switchover.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 OldMaster=$1
 NewMaster=$2
@@ -10,17 +10,27 @@ docker exec -it proxysql-merch mysql -u admin -padmin -h 127.0.0.1 -P6032 -e "LO
 sleep 1
 echo "Here python script changes master and slave"
 python switch_master.py $OldMaster $NewMaster
+echo "Python script is finished working"
+
+echo "Is your master dead?(yes/no): "
+read answer
 
 sleep 1
 echo "Delete write old master"
 docker exec -it proxysql-merch mysql -u admin -padmin -h 127.0.0.1 -P6032 -e "DELETE FROM mysql_servers where hostgroup_id=0 and hostname=\"$OldMaster\";"
 docker exec -it proxysql-merch mysql -u admin -padmin -h 127.0.0.1 -P6032 -e "LOAD MYSQL SERVERS TO RUNTIME;"
 docker exec -it proxysql-merch mysql -u admin -padmin -h 127.0.0.1 -P6032 -e "SAVE MYSQL SERVERS TO DISK;"
+
 sleep 1
 echo "Change status read old master"
-docker exec -it proxysql-merch mysql -u admin -padmin -h 127.0.0.1 -P6032 -e "UPDATE mysql_servers SET status='ONLINE' WHERE hostgroup_id=1 AND hostname=\"$OldMaster\";"
+if [[ $answer = "yes" ]]; then
+  docker exec -it proxysql-merch mysql -u admin -padmin -h 127.0.0.1 -P6032 -e "UPDATE mysql_servers SET status='SHUNNED' WHERE hostgroup_id=1 AND hostname=\"$OldMaster\";"
+elif [[ $answer = "no" ]]; then
+  docker exec -it proxysql-merch mysql -u admin -padmin -h 127.0.0.1 -P6032 -e "UPDATE mysql_servers SET status='ONLINE' WHERE hostgroup_id=1 AND hostname=\"$OldMaster\";"  
+fi
 docker exec -it proxysql-merch mysql -u admin -padmin -h 127.0.0.1 -P6032 -e "LOAD MYSQL SERVERS TO RUNTIME;"
 docker exec -it proxysql-merch mysql -u admin -padmin -h 127.0.0.1 -P6032 -e "SAVE MYSQL SERVERS TO DISK;"
+
 sleep 1
 echo "---New tables---"
 docker exec -it proxysql-merch mysql -u admin -padmin -h 127.0.0.1 -P6032 -e "SELECT * from mysql_servers;"
