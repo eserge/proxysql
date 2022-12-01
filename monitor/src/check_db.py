85a23258edb5ac3103e7725a6c34db077ae594bf
Serge <neserge@gmail.com>
2022-12-01 15:24:16 +0300

    check_db.py: add cli arguments

--- monitor/src/check_db.py
+++ monitor/src/check_db.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys
 import mysql.connector
 
@@ -8,17 +9,30 @@ try:
 except IndexError:
     HOST = "192.168.96.2"
 try:
-    PORT = args_list[2]
+    PORT = int(args_list[2])
 except IndexError:
     PORT =  6033
+try:
+    USER = args_list[3]
+except IndexError:
+    USER = "sysbench"
+try:
+    PASSWORD = args_list[4]
+except IndexError:
+    PASSWORD = "sysbench"
+try:
+    DB = args_list[5]
+except IndexError:
+    DB = "sysbench"
 
 
+print(f"Trying to connect to {USER}:{PASSWORD}@{HOST}:{PORT}/{DB}")
 db2 = mysql.connector.connect(
     host=HOST,
     port=PORT,
-    user="sysbench",
-    password="sysbench",
-    db="crypto_db"
+    user=USER,
+    password=PASSWORD,
+    db=DB,
 )
 
 cursor2 = db2.cursor()
