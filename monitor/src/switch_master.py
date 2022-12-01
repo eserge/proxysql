fdf20d9b186a2848e84ff07dfa1d1ec09b8c3cdf
Serge <neserge@gmail.com>
2022-12-01 18:02:46 +0300

    switch_master.py: handle InterfaceError, fix naming

--- monitor/src/switch_master.py
+++ monitor/src/switch_master.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 import mysql.connector
-from mysql.connector.errors import DatabaseError
+from mysql.connector.errors import DatabaseError, InterfaceError
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -28,19 +28,19 @@ def connect_db(host, port, user, password):
 
 try:
     db_old_master = connect_db(old_master_host, db_port, db_user, db_pass)
-except DatabaseError:
+except (DatabaseError, InterfaceError):
     print('---Master is dead---')
 else:
-    cursor = db_old_master.cursor()
+    cursor_old_master = db_old_master.cursor()
     print('---Setting old master to slave status---')
-    cursor.execute("STOP SLAVE;")
-    cursor.execute(f"CHANGE MASTER TO MASTER_HOST='{new_master_host}', MASTER_USER='repl', MASTER_PASSWORD='{slavepass}';")
-    cursor.execute("START SLAVE;")
-    cursor.execute("FLUSH TABLES WITH READ LOCK;")
-    cursor.execute("SET GLOBAL read_only=ON;")
-    cursor.execute("UNLOCK TABLES;")
-    cursor.execute(r"SHOW SLAVE status;")
-    res = cursor.fetchall()
+    cursor_old_master.execute("STOP SLAVE;")
+    cursor_old_master.execute(f"CHANGE MASTER TO MASTER_HOST='{new_master_host}', MASTER_USER='repl', MASTER_PASSWORD='{slavepass}';")
+    cursor_old_master.execute("START SLAVE;")
+    cursor_old_master.execute("FLUSH TABLES WITH READ LOCK;")
+    cursor_old_master.execute("SET GLOBAL read_only=ON;")
+    cursor_old_master.execute("UNLOCK TABLES;")
+    cursor_old_master.execute(r"SHOW SLAVE status;")
+    res = cursor_old_master.fetchall()
     print('---Slave status---')
     print(res)
 
@@ -49,26 +49,26 @@ db_new_master = connect_db(new_master_host, db_port, db_user, db_pass)
 db_replica = connect_db(replica_host, db_port, db_user, db_pass)
 
 
-cursor2 = db_new_master.cursor()
-cursor3 = db_replica.cursor()
+cursor_new_master = db_new_master.cursor()
+cursor_replica = db_replica.cursor()
 print('---Starting set up a new master---')
-cursor2.execute("STOP SLAVE;")
-cursor2.execute("RESET MASTER;")
-cursor2.execute(f"CREATE USER repl@'{old_master_host}' IDENTIFIED VIA mysql_native_password USING PASSWORD('{slavepass}');")
-cursor2.execute(f"GRANT REPLICATION SLAVE ON *.* TO repl@'{old_master_host}';")
-cursor2.execute("FLUSH PRIVILEGES;")
-cursor2.execute("FLUSH TABLES WITH READ LOCK;")
-cursor2.execute("SET GLOBAL read_only=OFF;")
-cursor2.execute("UNLOCK TABLES;")
-cursor2.execute("SHOW MASTER status;")
-res = cursor2.fetchall()
+cursor_new_master.execute("STOP SLAVE;")
+cursor_new_master.execute("RESET MASTER;")
+cursor_new_master.execute(f"CREATE USER repl@'{old_master_host}' IDENTIFIED VIA mysql_native_password USING PASSWORD('{slavepass}');")
+cursor_new_master.execute(f"GRANT REPLICATION SLAVE ON *.* TO repl@'{old_master_host}';")
+cursor_new_master.execute("FLUSH PRIVILEGES;")
+cursor_new_master.execute("FLUSH TABLES WITH READ LOCK;")
+cursor_new_master.execute("SET GLOBAL read_only=OFF;")
+cursor_new_master.execute("UNLOCK TABLES;")
+cursor_new_master.execute("SHOW MASTER status;")
+res = cursor_new_master.fetchall()
 print('---Master status---')
 print(res)
 print('---Indicating replica to a new master---')
-cursor3.execute("STOP SLAVE;")
-cursor3.execute(f"CHANGE MASTER TO MASTER_HOST='{new_master_host}', MASTER_USER='repl', MASTER_PASSWORD='{slavepass}';")
-cursor3.execute("START SLAVE;")
-cursor3.execute(r"SHOW SLAVE status;")
-res = cursor3.fetchall()
+cursor_replica.execute("STOP SLAVE;")
+cursor_replica.execute(f"CHANGE MASTER TO MASTER_HOST='{new_master_host}', MASTER_USER='repl', MASTER_PASSWORD='{slavepass}';")
+cursor_replica.execute("START SLAVE;")
+cursor_replica.execute(r"SHOW SLAVE status;")
+res = cursor_replica.fetchall()
 print('---Slave status---')
 print(res)
