d5dbbd157c10e81bf508315e5b261d79b46d4327
Serge <neserge@gmail.com>
2022-12-07 16:28:47 +0300

    Show number of connections on each host

--- monitor/src/testbench.py
+++ monitor/src/testbench.py
@@ -2,12 +2,14 @@ import logging
 import sys
 import time
 
+from collections import Counter
+
 import mysql.connector
 from mysql.connector.errors import DatabaseError, InterfaceError
 
 logger = logging.getLogger("proxysql")
-logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler(sys.stdout))
+logger.setLevel(logging.INFO)
 
 args_list = sys.argv
 try:
@@ -22,6 +24,8 @@ except IndexError:
 
 USER = "sysbench"
 PASS = "sysbench"
+
+hosts = Counter()
 
 
 def connect(host, port, user, password):
@@ -43,17 +47,21 @@ def select_hostname(connection):
 
 
 def main():
-    while True:
-        try:
-            connection = connect(HOST, PORT, USER, PASS)
-        except (DatabaseError, InterfaceError):
-            logger.exception("Couldn't connect")
-            return 1
-        logger.info(connection)
+    try:
+        while True:
+            try:
+                connection = connect(HOST, PORT, USER, PASS)
+            except (DatabaseError, InterfaceError):
+                logger.exception("Couldn't connect")
+                return 1
+            logger.debug(connection)
 
-        hostname = select_hostname(connection)
-        print(hostname)
-        time.sleep(0.5)
+            hostname = select_hostname(connection)
+            hosts[hostname] += 1
+            logger.info(f"Hostname: {hostname} : {hosts[hostname]} times")
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        logger.info(hosts)
 
     return 0
 
